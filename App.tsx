3e96c8d1032e55692e72cab2435d7e5add708890
GSFZamai <gsfzamai@gmail.com>
2025-05-28 21:40:59 -0300

    init(Project): Created a React Native CLI Project

---
+++ App.tsx
@@ -0,0 +1,15 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {View} from 'react-native';
+
+function App(): React.JSX.Element {
+  return <View style={{flex: 1}}></View>;
+}
+
+export default App;
